49fd2ecd22c7300ccea73db24c09c59569464a32
Ben Gibbs <ben@bobop.co.uk>
2015-10-25 06:29:22 +0000

    made internal links use https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def current_host
-    Rails.env.development? ? "http://localhost:3000" : "http://gawi-app.heroku.com"
+    Rails.env.development? ? "http://localhost:3000" : "https://gawi-app.heroku.com"
   end
 
 end
