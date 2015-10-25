83af50b4811b0d7983cb11c263da927bf827ad80
Bruce Wilson <b.wilson@volcanic.co.uk>
2015-10-25 01:27:32 +0000

    index styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024204838) do
+ActiveRecord::Schema.define(version: 20151024210815) do
 
   create_table "admin_wards", force: :cascade do |t|
     t.string   "name",                  limit: 255
