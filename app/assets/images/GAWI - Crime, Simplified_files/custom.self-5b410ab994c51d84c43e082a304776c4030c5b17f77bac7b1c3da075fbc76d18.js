ef790318fc356fb0c71727dc06c959084460c353
Mark Brewster <m.brewster@volcanic.co.uk>
2015-10-25 09:48:14 +0000

    avatars

--- app/assets/images/GAWI - Crime, Simplified_files/custom.self-5b410ab994c51d84c43e082a304776c4030c5b17f77bac7b1c3da075fbc76d18.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var config = {
-    '#search_category' : {disable_search_threshold:10}
-    }
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
-});
