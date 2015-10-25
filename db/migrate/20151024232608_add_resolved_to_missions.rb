aaf254097f490359b00009a062d7e8997935a8a0
Ben Gibbs <ben@bobop.co.uk>
2015-10-25 01:27:02 +0000

    made sure boolean default is false

--- db/migrate/20151024232608_add_resolved_to_missions.rb
+++ db/migrate/20151024232608_add_resolved_to_missions.rb
@@ -1,5 +1,5 @@
 class AddResolvedToMissions < ActiveRecord::Migration
   def change
-    add_column :missions, :resolved, :boolean, default: 0
+    add_column :missions, :resolved, :boolean, default: false
   end
 end
